96ba09780bb7c75beb354da64081217f3508ded1
Ostrich96 <48691609+Ostrich96@users.noreply.github.com>
2022-01-30 17:09:42 -0500

    test

--- test.py
+++ test.py
@@ -4,13 +4,14 @@ from keras.models import Model
 
 import matplotlib.pyplot as plt
 
+tf.compat.v1.disable_eager_execution()
 
 x = tf.Variable(3, name="x") 
 y = tf.Variable(4, name="y") 
 f=x*x*y+y+2
 
 
-sess = tf.Session()
+sess = tf.compat.v1.Session()
 sess.run(x.initializer)
 sess.run(y.initializer)
 result = sess.run(f)
---
+++ test2.py
@@ -0,0 +1,13 @@
+import numpy as np
+#Linear Regression with TensorFlow
+import tensorflow as tf
+from sklearn.datasets import fetch_california_housing
+housing = fetch_california_housing()
+m, n = housing.data.shape
+housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
+X = tf.constant(housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+XT = tf.transpose(X)
+theta = tf.matmul(tf.matmul(tf.linalg.inv(tf.matmul(XT, X)), XT), y)
+with tf.compat.v1.Session() as sess:
+ theta_value = theta.eval()
